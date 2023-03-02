c8667bd4fd0aee4f99daeace10483070c26450b3
Kamlesh Shah <Kamlesh Shah@LAPTOP-JL6INC90>
2023-03-01 18:21:14 -0600

    I am uploading all files to github

---
+++ src/test/Javaproject.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Javaproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println(30+40);
+
+	}
+
+}
